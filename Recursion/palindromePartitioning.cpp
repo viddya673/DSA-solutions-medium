5541fe135ac220085a09e9c74b6ff920da4b4a60
K Viddya <62757458+viddya673@users.noreply.github.com>
2023-11-14 16:44:15 +0530

    Recursion day 4

---
+++ Recursion/palindromePartitioning.cpp
@@ -0,0 +1,29 @@
+bool isPalindrome(string s, int start, int end){
+    while(start<=end){
+        if(s[start++]!=s[end--]) 
+            return false;
+    }
+    return true;
+}
+
+void helper(int ind, string s, vector<string> temp, vector<vector<string>>& res){
+    if(ind==s.length()){
+        res.push_back(temp);
+        return;
+    }
+
+    for(int i=ind; i<s.length(); i++){
+        if(isPalindrome(s, ind, i)){
+            temp.push_back(s.substr(ind, i-ind+1));
+            helper(i+1, s, temp, res);
+            temp.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string s) {
+    vector<string> temp;
+    vector<vector<string>> res;
+    helper(0, s, temp, res);
+    return res;
+}
