e37bc1ff2fcd2e71cb90ea0a2bd949d6f0a2edac
K Viddya <62757458+viddya673@users.noreply.github.com>
2024-01-10 11:16:26 +0530

    modification

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -10,19 +10,6 @@ vector<int> bfsTraversal(int n, vector<vector<int>> &adj){
     int root=0;
     q.push(root);
     visited[0] = 1;
-
-    // while(!q.empty()){
-    //     int curr = q.front();
-    //     res.push_back(curr);
-    //     q.pop();
-        
-    //     for(int a: adj[curr]){
-    //         if(visited[a]==0){
-    //             q.push(a);
-    //             visited[a] = 1;
-    //         }
-    //     }
-    // }
     while(!q.empty()){
         int l_size = q.size();
 
@@ -42,10 +29,40 @@ vector<int> bfsTraversal(int n, vector<vector<int>> &adj){
     return res;
 }
 
+vector<int> bfsGridTraversal(int n, vector<vector<int>> adj){
+    int grid[n][n] = {0};
+    for(int i=0; i<n; i++){
+        int u = i;
+        for(int v: adj[i]){
+            grid[u][v] = 1;
+        }
+    }
+
+    queue<int> q;
+    vector<int> vis(n, 0);
+    vector<int> res;
+
+    q.push(0);
+    vis[0] = 1;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        res.push_back(u);
+
+        for(int v=0; v<n; v++){
+            if(grid[u][v]==1 && !vis[v]){
+                q.push(v);
+                vis[v] = 1;
+            }
+        }
+    }
+    return res;
+}
+
 int main(){
-    vector<vector<int>> adj{{1,2,3}, {4}, {5}, {}, {}, {}};
+    vector<vector<int>> adj{{1,3}, {0, 2}, {1, 5}, {0, 4}, {3, 6}, {2, 6, 7}, {4, 5}, {5}};
     int n = adj.size();
-    vector<int> res = bfsTraversal(n, adj);
+    vector<int> res = bfsGridTraversal(n, adj);
     for(int r: res){
         cout<<r<<endl;
     }
